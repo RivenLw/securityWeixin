e232b1e9c5d49ee6468dcf4055efbe7ce59cf7f2
RivenLw <RivenLw@github.com>
2018-05-29 15:56:21 +0800

    答辩结束

--- src/com/Riven/ssm/util/Peizhi.java
+++ src/com/Riven/ssm/util/Peizhi.java
@@ -3,6 +3,6 @@ package com.Riven.ssm.util;
 public class Peizhi {
 
 	//外网映射ip
-	public static String waiUrl = "369c320c.ngrok.io";//"10f3d3e9.ngrok.io";
+	public static String waiUrl = "187af92a.ngrok.io";//"10f3d3e9.ngrok.io";
 	
 }
